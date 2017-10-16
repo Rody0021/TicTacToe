d81864869f7dbda9ed0244bc8ebb15801cb4879f
Rodolfo Olivares <fifa0014@yahoo.com>
2017-10-16 12:38:53 -0500

    Assembly project Game 10/16

--- tictt.asm
+++ tictt.asm
@@ -1,10 +1,10 @@
 .data	
 	Board: .ascii "    A  B  C     \n"
-		      " A   |   |  \n"
+		      " 1   |   |  \n"
 		      "   --+---+--\n"
-	 	      " B   |   | \n"
+	 	      " 2   |   | \n"
 	     	      "   --+---+--\n"
-	              " C   |   |\n"
+	              " 3   |   |\n"
 	
 	 
 	 sp: .asciiz "\n"
@@ -17,15 +17,16 @@
 	 tie: .asciiz  "\nTie!!!"
 	gameMenu: .asciiz "\n\nChoose an option:\n[1] New Game\t[99] Quit\nOption: "
 	clean: .byte ' '
-	AA: .asciiz "AA"
-	BA: .asciiz "BA"
-	CA: .asciiz "CA"
-	AB: .asciiz "AB"
-	BB: .asciiz "BB"
-	CB: .asciiz "CB"
-	AC: .asciiz "AC"
-	BC: .asciiz "BC"
-	CC: .asciiz "CC"
+	A1: .asciiz "A1"
+	B1: .asciiz "B1"
+	C1: .asciiz "C1"
+	A2: .asciiz "A2"
+	B2: .asciiz "B2"
+	C2: .asciiz "C2"
+	A3: .asciiz "A3"
+	B3: .asciiz "B3"
+	C3: .asciiz "C3"
+	player_move: .space 9
 	   
 .text
 
@@ -50,6 +51,16 @@ main:
 
 			lb $a1, clean
 			
+			#sb $a1, 14($s1)
+			#sb $a1, 18($s1)
+			#sb $a1, 22($s1)
+			#sb $a1, 40($s1)
+			#sb $a1, 44($s1)
+			#sb $a1, 48($s1)
+			#sb $a1, 66($s1)
+			#sb $a1, 70($s1)
+			#sb $a1, 74($s1)
+			
 PrintBoard:
 	li $v0, 4
 	la $a0, Board
@@ -65,27 +76,34 @@ Play:
 			li $v0, 4
 			la $a0, askMove
 			syscall
+			li $v0, 8
+			la $a0, player_move
+			la $a1, 4
+			syscall
 			
-			beq $s6, AA, JAA
-			beq $s6, BA, JBA
-			beq $s6, CA, JCA
-			beq $s6, AB, JAB
-			beq $s6, BB, JBB
-			beq $s6, CB, JCB
-			beq $s6, AC, JAC
-			beq $s6, BC, JBC
-			beq $s6, CC, JCC
+			la $s6, player_move
+			
+			beq $s6, A1, JA1
+			#beq $s6, B1, JB1
+			#beq $s6, C1, JC1
+			#beq $s6, A2, JA2
+			#beq $s6, B2, JB2
+			#beq $s6, C2, JC2
+			#beq $s6, A3, JA3
+			#beq $s6, B3, JB3
+			#beq $s6, C3, JC3
 			
 			li $v0, 4
 			la $a0, invalidMove
 			syscall
+			b Play
 			
+JA1:
+	li $v0, 4
+	la $a0, A1
+	syscall
 			
-			
-JAA:  
-			
-	
-	
-	
+
+
 	li $v0, 10
 	syscall
