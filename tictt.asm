e2e1711e57beb911c4e1b5d7a515a4fb245bf4a2
Rodolfo Olivares <fifa0014@yahoo.com>
2017-10-18 12:35:41 -0500

    TTT

--- tictt.asm
+++ tictt.asm
@@ -10,22 +10,17 @@
 	 sp: .asciiz "\n"
          xplayer: .asciiz  "X"
 	 oplayer: .asciiz "O"   
-	 askMove: .asciiz "Player insert your play (column|line)\n"
+	 askMove: .asciiz "Player insert your play (column|line):\n"
 	 invalidMove: .asciiz "Invalid Move"
 	 occupiedSpace: .asciiz "Space already occupied\n"    
 	 won: .asciiz "\nPlayer   Won! \n"
 	 tie: .asciiz  "\nTie!!!"
 	gameMenu: .asciiz "\n\nChoose an option:\n[1] New Game\t[99] Quit\nOption: "
 	clean: .byte ' '
-	A1: .asciiz "A1"
-	B1: .asciiz "B1"
-	C1: .asciiz "C1"
-	A2: .asciiz "A2"
-	B2: .asciiz "B2"
-	C2: .asciiz "C2"
-	A3: .asciiz "A3"
-	B3: .asciiz "B3"
-	C3: .asciiz "C3"
+	A: .asciiz "A"
+	B: .asciiz "B"
+	C: .asciiz "C"
+	
 	player_move: .space 9
 	   
 .text
@@ -51,15 +46,15 @@ main:
 
 			lb $a1, clean
 			
-			#sb $a1, 14($s1)
-			#sb $a1, 18($s1)
-			#sb $a1, 22($s1)
-			#sb $a1, 40($s1)
-			#sb $a1, 44($s1)
-			#sb $a1, 48($s1)
-			#sb $a1, 66($s1)
-			#sb $a1, 70($s1)
-			#sb $a1, 74($s1)
+			sb $a1, 14($s1)
+			sb $a1, 18($s1)
+			sb $a1, 22($s1)
+			sb $a1, 40($s1)
+			sb $a1, 44($s1)
+			sb $a1, 48($s1)
+			sb $a1, 66($s1)
+			sb $a1, 70($s1)
+			sb $a1, 74($s1)
 			
 PrintBoard:
 	li $v0, 4
@@ -69,7 +64,7 @@ PrintBoard:
 			lb $a1, xplayer
 			
 			
-OPlatyer:
+OPlayer:
 			lb $a1, oplayer
 			
 Play:
@@ -83,7 +78,7 @@ Play:
 			
 			la $s6, player_move
 			
-			beq $s6, A1, JA1
+			#beq $s6, A1, JA1
 			#beq $s6, B1, JB1
 			#beq $s6, C1, JC1
 			#beq $s6, A2, JA2
@@ -93,15 +88,130 @@ Play:
 			#beq $s6, B3, JB3
 			#beq $s6, C3, JC3
 			
-			li $v0, 4
-			la $a0, invalidMove
-			syscall
-			b Play
+			#li $v0, 4
+			#la $a0, invalidMove
+			#syscall
+			# Play
 			
+
+
+
+
 JA1:
-	li $v0, 4
-	la $a0, A1
-	syscall
+			bnez $t1, Occupied
+			bnez $t0, OA1
+
+			XA1:
+			li $t1, 1
+			sb $a1, 14($s1)
+			
+			OA1:
+			li $t1, 2
+			sb $a1, 14($s1)	
+			
+JB1:
+			bnez $t1, Occupied
+			bnez $t0, OB1
+
+			XB1:
+			li $t1, 1
+			sb $a1, 18($s1)
+			
+			OB1:
+			li $t1, 2
+			sb $a1, 18($s1)	
+			
+JC1:
+			bnez $t1, Occupied
+			bnez $t0, OC1
+
+			XC1:
+			li $t1, 1
+			sb $a1, 22($s1)
+			
+			OC1:
+			li $t1, 2
+			sb $a1, 22($s1)	
+			
+JA2:
+			bnez $t1, Occupied
+			bnez $t0, JA2
+
+			XA2:
+			li $t1, 1
+			sb $a1, 40($s1)
+			
+			OA2:
+			li $t1, 2
+			sb $a1, 40($s1)
+			
+JB2:
+			bnez $t1, Occupied
+			bnez $t0, JB2
+
+			XB2:
+			li $t1, 1
+			sb $a1, 44($s1)
+			
+			OB2:
+			li $t1, 2
+			sb $a1, 44($s1)
+
+JC2:
+			bnez $t1, Occupied
+			bnez $t0, JC2
+
+			XC2:
+			li $t1, 1
+			sb $a1, 48($s1)
+			
+			OC2:
+			li $t1, 2
+			sb $a1, 48($s1)
+			
+JA3:
+			bnez $t1, Occupied
+			bnez $t0, JA3
+
+			XA3:
+			li $t1, 1
+			sb $a1, 66($s1)
+			
+			OA3:
+			li $t1, 2
+			sb $a1, 66($s1)
+			
+JB3:
+			bnez $t1, Occupied
+			bnez $t0, JB3
+
+			XB3:
+			li $t1, 1
+			sb $a1, 70($s1)
+			
+			OB3:
+			li $t1, 2
+			sb $a1, 70($s1)
+			
+JC3:
+			bnez $t1, Occupied
+			bnez $t0, JC3
+
+			XC3:
+			li $t1, 1
+			sb $a1, 74($s1)
+			
+			OC3:
+			li $t1, 2
+			sb $a1, 74($s1)
+
+Occupied:
+			li $v0, 4
+			la $a0, occupiedSpace
+			syscall
+			j Play			
+			
+	
 			
 
 
